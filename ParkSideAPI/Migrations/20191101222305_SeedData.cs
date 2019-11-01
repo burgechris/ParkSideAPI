056d946fa3cfe6efcb9004e0bd24c516d27c6377
Chris Burge <topher.burge@gmail.com>
2019-11-01 15:23:48 -0700

    seed more parks into the database.

---
+++ ParkSideAPI/Migrations/20191101222305_SeedData.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParkSideAPI.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Parks",
+                columns: new[] { "ParkId", "Area", "Coordinates", "Established", "GoverningBody", "Name", "StateLocation", "Website" },
+                values: new object[,]
+                {
+                    { 2, "49,075.26 acres", "44.35°N 68.21°W", 1919, "National Park Service", "Acadia National Park", "Maine", "https://www.nps.gov/acad/index.htm" },
+                    { 3, "183,224.05 acres", "42.94°N 122.1°W", 1902, "National Park Service", "Crater Lake National Park", "Oregon", "https://www.nps.gov/crla/index.htm" },
+                    { 4, "3,372,981.42  acres", "36.24°N 116.82°W", 1994, "National Park Service", "Death Valley Lake National Park", "California", "https://www.nps.gov/deva/index.htm" },
+                    { 5, "1,013,125.99  acres", "48.80°N 114.00°W", 1910, "National Park Service", "Glacier Lake National Park", "Montana", "https://www.nps.gov/glac/index.htm" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 5);
+        }
+    }
+}
--- ParkSideAPI/Models/ParkSideAPIContext.cs
+++ ParkSideAPI/Models/ParkSideAPIContext.cs
@@ -10,12 +10,20 @@ namespace ParkSideAPI.Models
     }
 
     public DbSet<Park> Parks { get; set; }
-    
+
     protected override void OnModelCreating (ModelBuilder builder)
     {
           builder.Entity<Park>()
           .HasData(
-              new Park { ParkId = 1, Name = "Yellowstone National Park", GoverningBody = "National Park Service", StateLocation = "Wyoming", Established = 1872, Coordinates = "44°36′N 110°30′W", Area = "2,219,791 acres", Website = "https://www.nps.gov/yell/index.htm" }
+              new Park { ParkId = 1, Name = "Yellowstone National Park", GoverningBody = "National Park Service", StateLocation = "Wyoming", Established = 1872, Coordinates = "44°36′N 110°30′W", Area = "2,219,791 acres", Website = "https://www.nps.gov/yell/index.htm" },
+
+              new Park { ParkId = 2, Name = "Acadia National Park", GoverningBody = "National Park Service", StateLocation = "Maine", Established = 1919, Coordinates = "44.35°N 68.21°W", Area = "49,075.26 acres", Website = "https://www.nps.gov/acad/index.htm" },
+
+              new Park { ParkId = 3, Name = "Crater Lake National Park", GoverningBody = "National Park Service", StateLocation = "Oregon", Established = 1902, Coordinates = "42.94°N 122.1°W", Area = "183,224.05 acres", Website = "https://www.nps.gov/crla/index.htm" },
+
+              new Park { ParkId = 4, Name = "Death Valley Lake National Park", GoverningBody = "National Park Service", StateLocation = "California", Established = 1994, Coordinates = "36.24°N 116.82°W", Area = "3,372,981.42  acres", Website = "https://www.nps.gov/deva/index.htm" },
+
+              new Park { ParkId = 5, Name = "Glacier Lake National Park", GoverningBody = "National Park Service", StateLocation = "Montana", Established = 1910, Coordinates = "48.80°N 114.00°W", Area = "1,013,125.99  acres", Website = "https://www.nps.gov/glac/index.htm" }
           );
     }
   }
