e037cd089694c99e461fcc2377d51dc8efdb22d9
Chris Burge <topher.burge@gmail.com>
2019-11-01 15:58:39 -0700

    seed a few more parks into the database.

---
+++ ParkSideAPI/Migrations/20191101225754_SeedData2.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParkSideAPI.Migrations
+{
+    public partial class SeedData2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 4,
+                column: "Area",
+                value: "3,372,981.42 acres");
+
+            migrationBuilder.UpdateData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 5,
+                column: "Area",
+                value: "1,013,125.99 acres");
+
+            migrationBuilder.InsertData(
+                table: "Parks",
+                columns: new[] { "ParkId", "Area", "Coordinates", "Established", "GoverningBody", "Name", "StateLocation", "Website" },
+                values: new object[,]
+                {
+                    { 6, "310,044.36 acres", "43.73°N 110.80°W", 1929, "National Park Service", "Grand Teton Lake National Park", "Wyoming", "https://www.nps.gov/grte/index.htm" },
+                    { 7, "325,605.28 acres", "20.72°N 156.17°W", 1916, "National Park Service", "Haleakala National Park", "Hawaii", "https://www.nps.gov/hale/index.htm" },
+                    { 8, "790,635.74 acres", "33.79°N 115.90°W", 1994, "National Park Service", "Joshua Tree National Park", "California", "https://www.nps.gov/jotr/index.htm" },
+                    { 9, "52,485.17 acres", "37.18°N 108.49°W", 1906, "National Park Service", "Mesa Verde National Park", "Colorado", "https://www.nps.gov/meve/index.htm" },
+                    { 10, "504,780.94 acres", "48.70°N 121.20°W", 1968, "National Park Service", "North Cascades National Park", "Washington", "https://www.nps.gov/noca/index.htm" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 10);
+
+            migrationBuilder.UpdateData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 4,
+                column: "Area",
+                value: "3,372,981.42  acres");
+
+            migrationBuilder.UpdateData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 5,
+                column: "Area",
+                value: "1,013,125.99  acres");
+        }
+    }
+}
--- ParkSideAPI/Models/ParkSideAPIContext.cs
+++ ParkSideAPI/Models/ParkSideAPIContext.cs
@@ -21,9 +21,19 @@ namespace ParkSideAPI.Models
 
               new Park { ParkId = 3, Name = "Crater Lake National Park", GoverningBody = "National Park Service", StateLocation = "Oregon", Established = 1902, Coordinates = "42.94°N 122.1°W", Area = "183,224.05 acres", Website = "https://www.nps.gov/crla/index.htm" },
 
-              new Park { ParkId = 4, Name = "Death Valley Lake National Park", GoverningBody = "National Park Service", StateLocation = "California", Established = 1994, Coordinates = "36.24°N 116.82°W", Area = "3,372,981.42  acres", Website = "https://www.nps.gov/deva/index.htm" },
+              new Park { ParkId = 4, Name = "Death Valley Lake National Park", GoverningBody = "National Park Service", StateLocation = "California", Established = 1994, Coordinates = "36.24°N 116.82°W", Area = "3,372,981.42 acres", Website = "https://www.nps.gov/deva/index.htm" },
 
-              new Park { ParkId = 5, Name = "Glacier Lake National Park", GoverningBody = "National Park Service", StateLocation = "Montana", Established = 1910, Coordinates = "48.80°N 114.00°W", Area = "1,013,125.99  acres", Website = "https://www.nps.gov/glac/index.htm" }
+              new Park { ParkId = 5, Name = "Glacier Lake National Park", GoverningBody = "National Park Service", StateLocation = "Montana", Established = 1910, Coordinates = "48.80°N 114.00°W", Area = "1,013,125.99 acres", Website = "https://www.nps.gov/glac/index.htm" },
+
+              new Park { ParkId = 6, Name = "Grand Teton Lake National Park", GoverningBody = "National Park Service", StateLocation = "Wyoming", Established = 1929, Coordinates = "43.73°N 110.80°W", Area = "310,044.36 acres", Website = "https://www.nps.gov/grte/index.htm" },
+
+              new Park { ParkId = 7, Name = "Haleakala National Park", GoverningBody = "National Park Service", StateLocation = "Hawaii", Established = 1916, Coordinates = "20.72°N 156.17°W", Area = "325,605.28 acres", Website = "https://www.nps.gov/hale/index.htm" },
+
+              new Park { ParkId = 8, Name = "Joshua Tree National Park", GoverningBody = "National Park Service", StateLocation = "California", Established = 1994, Coordinates = "33.79°N 115.90°W", Area = "790,635.74 acres", Website = "https://www.nps.gov/jotr/index.htm" },
+
+              new Park { ParkId = 9, Name = "Mesa Verde National Park", GoverningBody = "National Park Service", StateLocation = "Colorado", Established = 1906, Coordinates = "37.18°N 108.49°W", Area = "52,485.17 acres", Website = "https://www.nps.gov/meve/index.htm" },
+
+              new Park { ParkId = 10, Name = "North Cascades National Park", GoverningBody = "National Park Service", StateLocation = "Washington", Established = 1968, Coordinates = "48.70°N 121.20°W", Area = "504,780.94 acres", Website = "https://www.nps.gov/noca/index.htm" }
           );
     }
   }
