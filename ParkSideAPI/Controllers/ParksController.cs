396f77ce0fbb5263a39177f0c77f4efe8cc98d0e
Chris Burge <topher.burge@gmail.com>
2019-11-01 09:08:43 -0700

    add ability to add parks to the database.

---
+++ ParkSideAPI/Controllers/ParksController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ParkSideAPI.Models;
+
+namespace ParkSideAPI.Controllers
+{
+    [ApiVersion("1.0")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParksController : ControllerBase
+    {
+        private ParkSideAPIContext _db;
+
+        public ParksController(ParkSideAPIContext db)
+        {
+        _db = db;
+        }
+
+        // GET api/parks
+        [HttpGet]
+        public ActionResult<IEnumerable<Park>> Get()
+        {
+            
+        }
+
+        // GET api/parks/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/parks
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/parks/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/parks/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
