23c13997f39d25fc604aff0536026d87263dcb74
Chris Burge <topher.burge@gmail.com>
2019-11-01 11:18:33 -0700

    add first database seeding.

--- ParkSideAPI/Models/ParkSideAPIContext.cs
+++ ParkSideAPI/Models/ParkSideAPIContext.cs
@@ -10,5 +10,13 @@ namespace ParkSideAPI.Models
     }
 
     public DbSet<Park> Parks { get; set; }
+    
+    protected override void OnModelCreating (ModelBuilder builder)
+    {
+          builder.Entity<Park>()
+          .HasData(
+              new Park { ParkId = 1, Name = "Yellowstone National Park", GoverningBody = "National Park Service", StateLocation = "Wyoming", Established = 1872, Coordinates = "44°36′N 110°30′W", Area = "2,219,791 acres", Website = "https://www.nps.gov/yell/index.htm" }
+          );
+    }
   }
 }
